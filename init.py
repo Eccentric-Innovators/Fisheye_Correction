dc3de9d8098bb33d7941faa3ef3a01984b327730
Malolan V <BlackXDragon@users.noreply.github.com>
2020-01-18 16:23:48 +1300

    Added video option to command line

--- init.py
+++ init.py
@@ -6,13 +6,18 @@ def cli():
 	pass
 
 @cli.command()
-@click.option('--src', help = 'Source of the video feed.', default = 0)
-@click.option('--quality', help = 'Quality to use:\n\t-> 480p (default)\n\t-> 720p\n\t-> 1080p', default="480p")
+@click.option('--src', '-s', help = 'Source of the video feed.', default = 0)
+@click.option('--quality', '-q', help = 'Quality to use:\n\t-> 480p (default)\n\t-> 720p\n\t-> 1080p', default="480p")
+@click.option('--video', '-v', help = 'Set this flag to capture a video.', is_flag = True)
 @click.argument('dest', required = False, default = 'output_{}.png') #  help = 'File format and path to store the images in. {} will be incremented for each image.\nExample:\n\t\t./foldername/image_{}.png'
-def capture(src, dest, quality):
+def capture(src, dest, video, quality):
 	"""Capture images or videos"""
-	from imgCapture import captureImg
-	captureImg(src, dest, quality)
+	if video:
+		from vidCapture import captureVid
+		captureVid(src, dest, quality)
+	else:
+		from imgCapture import captureImg
+		captureImg(src, dest, quality)
 
 if __name__ == '__main__':
 	cli()
--- vidCapture.py
+++ vidCapture.py
@@ -1,28 +1,38 @@
 import numpy as np
 import cv2
+import config
 
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
+def captureVid(src, dest, quality):
+    cap = cv2.VideoCapture(src)
+    cap.set(3,config.qualities[quality][0])
+    cap.set(4,config.qualities[quality][1])
+    cv2.namedWindow("Camera")
+    start = False
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output_480p.avi',fourcc, 20.0, (640,480))
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (config.qualities[quality][0],config.qualities[quality][1]))
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    #print(frame.shape)
-    if ret==True:
-        # frame = cv2.flip(frame,1)
-        out.write(frame)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        #print(frame.shape)
+        if ret==True:
+            # frame = cv2.flip(frame,1)
+            if start == True:
+                out.write(frame)
 
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imshow('Camera',frame)
+
+            k = cv2.waitKey(1)
+            if k%256 == 27:
+                break
+
+            if k%256 == 32:
+                start = not start
+        else:
             break
-    else:
-        break
 
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
